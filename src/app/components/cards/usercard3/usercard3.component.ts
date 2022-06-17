6103c9d567d7fae38ed318982349c4dd87d975aa
muthu48 <muthugrandpa@gmail.com>
2022-06-17 19:35:25 -0400

    new usercard component

---
+++ src/app/components/cards/usercard3/usercard3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usercard3',
+  templateUrl: './usercard3.component.html',
+  styleUrls: ['./usercard3.component.scss']
+})
+export class Usercard3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import {AutocompleteLibModule} from 'angular-ng-autocomplete';
 
 import {GlobalSearchComponent} from './components/global-search/global-search.component';
+import { Usercard1Component } from './components/cards/usercard1/usercard1.component';
 import {Usercard2Component} from './components/cards/usercard2/usercard2.component';
+import { Usercard3Component } from './components/cards/usercard3/usercard3.component';
 
 
 @NgModule({
@@ -14,11 +16,15 @@ import {Usercard2Component} from './components/cards/usercard2/usercard2.compone
   ],
   exports:[
     GlobalSearchComponent,
-    Usercard2Component
+    Usercard1Component,
+    Usercard2Component,
+    Usercard3Component
   ],
   declarations: [
     GlobalSearchComponent,
-    Usercard2Component
+    Usercard1Component,
+    Usercard2Component,
+    Usercard3Component
   ],
   providers:[
 
